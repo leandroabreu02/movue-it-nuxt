7876e6320e658ad31cb8c924654d2ffa402b4a39
leandroabreu13 <djleandro.cardoso@gmail.com>
2021-03-28 13:27:42 -0300

    ep6 commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="flex flex-col lg:flex-row flex-1 lg:flex-none lg:mt-16 sm:gap-x-10 md:gap-x-20">
+	<section class="flex flex-col lg:flex-row flex-1 lg:flex-none lg:mt-16 sm:gap-x-10 md:gap-x-20">
 		<div class="flex flex-col w-full lg:w-1/2">
 			<Profile />
 			<CompletedChallenges />
-			<Countdown @completed="getNewChallenge"/>
+			<Countdown @completed="getNewChallenge" />
 			<button
 				v-if="hasCountdownCompleted"
 				disabled
@@ -31,28 +31,23 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
+import Vue from 'vue';
 import { mapState, mapGetters, mapMutations } from 'vuex';
 import { Mutations as ChallengesMT } from '~/store/Challenges/types';
 import { Mutations as CountdownMT } from '~/store/Countdown/types';
-
 import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue';
 import Profile from '~/components/molecules/Profile.vue';
 import Countdown from '~/components/molecules/Countdown.vue';
 import Card from '~/components/organisms/Card.vue';
-
 import {
 	scrollToElement,
 	getRandomNumber,
 	playAudio,
 	sendNotification,
 } from '~/utils';
-
 interface Head {
 	title: string;
 }
-
 export default Vue.extend({
 	head (): Head {
 		return {
@@ -65,8 +60,8 @@ export default Vue.extend({
 		Profile,
 		Card,
 	},
-	mounted() {
-		if('Notification' in window) {
+	mounted () {
+		if ('Notification' in window) {
 			Notification.requestPermission();
 		}
 	},
@@ -83,23 +78,21 @@ export default Vue.extend({
 			setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`,
 			setCurrentChallengeIndex: `Challenges/${ChallengesMT.SET_CURRENT_CHALLENGE_INDEX}`,
 		}),
-		setCountdownState(flag: boolean) {
-			this.setCountdownHasCompleted(false),
+		setCountdownState (flag: boolean) {
+			this.setCountdownHasCompleted(false);
 			this.setCountdownIsActive(flag);
 		},
-		getNewChallenge() {
-			const index = getRandomNumber(0, this.challengesLength)
+		getNewChallenge () {
+			const index = getRandomNumber(0, this.challengesLength);
 			this.setCountdownHasCompleted(true);
 			this.setCurrentChallengeIndex(index);
-
-			if(Notification?.permission === 'granted') {
+			if (Notification?.permission === 'granted') {
 				playAudio('/notification.mp3');
-				sendNotification('New challenge!', {
+				sendNotification('New Challenge!', {
 					body: 'A new challenge has started! Go complete it!',
 					icon: '/favicon.png',
 				});
 			}
-
 			this.$nextTick(() => {
 				scrollToElement('#challenge');
 			});
